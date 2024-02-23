686790c3c924277c1baae92b5198de147fc31aaa
Elif Alinli <semraelifalinli@gmail.com>
2024-02-23 14:27:28 +0000

    small tailwind changes to match film&tv

--- src/components/pages/ShowDetailsPage.jsx
+++ src/components/pages/ShowDetailsPage.jsx
@@ -29,18 +29,15 @@ const ShowDetailsPage = () => {
       {type === "movie" ? (
         <>
           <Header props={`Here are some details about ${data.title}!`} />
-          <div className="flex flex-col">
+          <div className="flex flex-col object-scale-down items-center">
             <img
-              className="w-full h-full rounded-l object-scale-down"
+              className="mt-5 mb-3 rounded-l h-160 w-80"
               src={`http://image.tmdb.org/t/p/w500/${data.poster_path}`}
               alt={data.title}
             />
-            <h3 className="font-semibold">
-              Release year: {data.release_date.slice(0, 4)}
-            </h3>
-
-            <h3 className="mb-3 font-semibold">Overview:</h3>
-            <p className="text-left mb-5">{data.overview}</p>
+            <h3 className="font-semibold my-2">Release date: {data.release_date}</h3>
+            <span className="flow-root mb-1 font-semibold">Overview: </span>
+            <p className="flow-root text-left mb-5">{data.overview}</p>
           </div>
         </>
       ) : (
@@ -52,9 +49,7 @@ const ShowDetailsPage = () => {
               src={`http://image.tmdb.org/t/p/w500/${data.poster_path}`}
               alt={data.name}
             />
-            <h3 className="flow-root font-semibold my-2">
-              Release date: {data.first_air_date}
-            </h3>
+            <h3 className="font-semibold my-2"> Release date: {data.first_air_date} </h3>
             <span className="flow-root mb-1 font-semibold">Overview: </span>
             <p className="flow-root text-left mb-5">{data.overview}</p>
           </div>
